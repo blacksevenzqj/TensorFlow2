975b0a5819609c20d5478b02af9b6c30fbe97b52
blacksevenzqj <397353721@qq.com>
2020-02-09 11:26:50 +0800

    1111

--- 4_BasicOperation/11_Broadcast.py
+++ 4_BasicOperation/11_Broadcast.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 
+a = tf.random.normal([4, 32, 32, 3])
+b = tf.ones([3])
+c = tf.fill([32, 32, 1], 2.)
+d = tf.random.uniform([4, 1, 1, 1])
+
+(a+b).shape   # (4, 32, 32, 3)
+(a+c).shape   # (4, 32, 32, 3)
+(a+d).shape   # (4, 32, 32, 3)
